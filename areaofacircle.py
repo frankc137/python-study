c98563b134a353e1c4b13996dcb6600592b6987e
frankc137 <micahstash@gmail.com>
2024-10-14 16:54:08 +0300

    first commit

---
+++ areaofacircle.py
@@ -0,0 +1,4 @@
+radius=23
+pie=3.14
+area=pie*radius*radius
+print(area)
---
+++ intro.py
@@ -0,0 +1,19 @@
+#print("Frank Micah")
+# Used to display the python output on the terminal
+fname= "Frank Micah"
+fname= "Sponge Bob"
+print(fname)
+#write a program that adds 2 variables num1 10 and num2 20 and stores them in a variable called sum
+num1=10
+num2=20
+sum=num1+num2
+print(sum) 
+
+# declare 2 variables fname lname add them up to get fullname
+fname= "Frank"
+lname= "Micah"
+full_name=fname+" "+lname
+print (full_name)
+#Get an input of phone number on the terminal
+phonenumber=input("enter your phone number:")
+print(phonenumber)
