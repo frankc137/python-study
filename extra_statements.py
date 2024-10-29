ba3cb992cd45f9690c088952bac8e692344e0a08
frankc137 <micahstash@gmail.com>
2024-10-29 15:26:15 +0300

    extra_statements.py

--- extra_statements.py
+++ extra_statements.py
@@ -32,12 +32,44 @@ else:
 #Prints "Access Granted" if user_id is in valid_ids.
 #Prints "Access Denied" if user_id is not in valid_ids.
 
+valid_ids = [101, 102, 103]
+
+user_id = 105 
+
+if user_id in valid_ids:
+    print("Access Granted")
+else:
+    print("Access Denied")
+
 #4.Given a variable value that could be of any type, write a conditional statement that:
 #Prints "String Detected" if value is a string.
 #Prints "Integer Detected" if value is an integer.
 #Prints "Unknown Type" for any other type.
+value = "Hello, World!"  
+
+if type(value)==str:
+    print("String Detected")
+elif type(value)==int:
+    print("Integer Detected")
+else:
+    print("Unknown Type")
+
 
 #5.Given x = 7 and y = 14, write nested conditional statements that print:
 #"x and y are both even" if both x and y are even numbers.
 #"Only y is even" if only y is even.
 #"Neither x nor y are even" if both are odd.
+
+x = 7
+y = 14
+
+if x % 2 == 0:
+    if y % 2 == 0: 
+        print("x and y are both even")
+    else:
+        print("Only x is even")
+else:  
+    if y % 2 == 0: 
+        print("Only y is even")
+    else:
+        print("Neither x nor y are even")
